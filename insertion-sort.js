8a00211f752c029841fba61f870140eac5cce7d3
Dion Emary <105363001@edu.cbe.ab.ca>
2022-09-12 11:59:37 -0600

    Linear Done, Working on Insertion

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,10 +1,14 @@
 function insertionSort(array) {
-	/*
-	 ** For each element in the array, swap it with the element
-	 ** before it over and over as long as the element before it
-	 ** is bigger
-	 */
-
+    let arrLength = array.length;
+        for (let i = 1; i < arrLength; i++) {
+            let current = array[i];
+            let x = i-1; 
+            while ((x > -1) && (current < array[x])) {
+                array[x+1] = array[x];
+                x--;
+            }
+            array[x+1] = current;
+        }
 	return array;
 }
 
