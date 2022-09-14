f31435cdcf39d5be0909b4c0fb9943dea1cc9b1c
Dion Emary <105363001@edu.cbe.ab.ca>
2022-09-14 11:55:20 -0600

    Added comments

--- selection-sort.js
+++ selection-sort.js
@@ -1,12 +1,15 @@
 function selectionSort(array) {
 	for(let i = 0; i < array.length; i++){
+		// Loops one time per array spot to fully sort it
 		let min = i
 		for(let temp = i+1; temp < array.length; temp++){
+			// loops enough times to find the current lowest
 			if(array[temp] < array[min]) {
 				min = temp;
 			}
 		}
 		if(min != i) {
+			// If there is nothing lower than min then it swaps the first item and the min
 			let sorted = array[i];
 			array[i] = array[min];
 			array[min] = sorted;
