44c0c9e52764897822a0644a49485c5912cf7ca7
Dion Emary <105363001@edu.cbe.ab.ca>
2022-09-14 10:56:08 -0600

    Complete

--- selection-sort.js
+++ selection-sort.js
@@ -1,6 +1,17 @@
 function selectionSort(array) {
-
-penis
+	for(let i = 0; i < array.length; i++){
+		let min = i
+		for(let temp = i+1; temp < array.length; temp++){
+			if(array[temp] < array[min]) {
+				min = temp;
+			}
+		}
+		if(min != i) {
+			let sorted = array[i];
+			array[i] = array[min];
+			array[min] = sorted;
+		}
+	}
 	return array;
 }
 
